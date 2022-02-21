6385d0d5573ea17f4881407b8a7e46285d295086
Natalia Byzova <nata22011997@gmail.com>
2022-02-21 20:33:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let openBrackets = [];
+  let pairsOfBrackets = {};
+
+  for (let arr of bracketsConfig) {
+    openBrackets.push(arr[0]);
+    pairsOfBrackets[arr[1]] = arr[0];
+  }
+
+  for (i = 0; i < str.length; i++) {
+    if (pairsOfBrackets[str[i]] === str[i]) {
+      if (stack.indexOf(str[i]) >= 0) {
+        if (pairsOfBrackets[str[i]] === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      } else {
+        stack.push(str[i]);
+      }
+    } else {
+      if (openBrackets.includes(str[i])) {
+        stack.push(str[i]);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+        if (pairsOfBrackets[str[i]] === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length === 0 ? true : false;
+};
